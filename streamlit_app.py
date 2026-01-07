20f0381e51393a19707f4b36ce52e8d3b51b3d81
nstanley-ui <nstanley@demandbase.com>
2026-01-06 17:19:51 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -115,7 +115,7 @@ def find_or_create_ad_group(client, customer_id, campaign_id, ad_group_name):
         ad_group.name = ad_group_name
         ad_group.campaign = campaign_service.campaign_path(customer_id, campaign_id)
         ad_group.status = client.enums.AdGroupStatusEnum.ENABLED
-        ad_group.type_ = client.enums.AdGroupTypeEnum.DISPLAY_STANDARD
+        ad_group.type_ = client.enums.AdGroupTypeEnum.DISPLAY_RESPONSIVE
 
         
         response = ad_group_service.mutate_ad_groups(
